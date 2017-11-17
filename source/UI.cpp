bdd51a91a7d9bb3afa25060ca2099a5703b898f7
joonaoikarinen <joonaoikarinen@kamk.fi>
2017-11-17 15:27:19 +0200

    Shaderien lataus filuista

--- source/UI.cpp
+++ source/UI.cpp
@@ -1,5 +1,6 @@
 #include <graphics\UI.h>
-
+#include <fstream>
+#include <string>
 
 UI::UI()
 {
@@ -43,28 +44,68 @@ void checkCompileErrors(GLuint object, std::string type)
 
 GLuint UI::CreateShaderProgram()
 {
-	const char* vertex_shader =
-		"precision mediump float;"
-		"attribute vec3 vp;"
-		"void main() {"
-		"  gl_Position = vec4(vp, 1.0);"
-		"}";
+	// Shadereiden lataaminen tiedostoista:
+	// vertexshader.vert
+	// fragmentshader.frag
 
-	const char* fragment_shader =
-		"void main() {"
-		"gl_FragColor = vec4(0.5, 0.0, 0.5, 1.0);"
-		"}";
+	std::string vertex_shader_string;
+	// Aukaistaan shader filu
+	std::ifstream file("vertexshader.vert");
 
+	// Jos tiedosto voi hyvin
+	if(file.good())
+	{
+		// Korvataan stringi filussa olevalla kamalla
+		vertex_shader_string.assign((std::istreambuf_iterator<char>(file)),
+			(std::istreambuf_iterator<char>()));
+	}
+	else
+	{
+		// Heitet‰‰n erroria consoleen
+		std::cout << "Failed to open file: vertexshader.vert" << std::endl;
+	}
+
+	// Jos filu auki -> filu kiinni
+	if (file.is_open())
+		file.close();
+
+	
+	// T‰h‰n voitas helposti lis‰t‰ kamaa esim. vertex_shader_string.c_str() + fragment_shader_string.c_str()
+	// Muutetaan stringi muotoon miss‰ glShaderSource() haluaa sen
+	GLchar const* vertex_shader[] = { vertex_shader_string.c_str() };
+
+	std::string fragment_shader_string;
+	// Avataan toinen shader filu
+	file.open("fragmentshader.frag", std::fstream::in);
+
+	// Jos tiedosto voi hyvin
+	if (file.good())
+	{
+		// Stringi korvataan shader filu kamalla
+		fragment_shader_string.assign((std::istreambuf_iterator<char>(file)),
+			(std::istreambuf_iterator<char>()));
+	}
+	else
+	{
+		// Muuten erroria konsoleen
+		std::cout << "Failed to open file: fragmentshader.frag" << std::endl;
+	}
+
+	// Jos filu auki -> filu kiinni
+	if (file.is_open())
+		file.close();
+
+	// Muutetaan stringi muotoon miss‰ glShaderSource() haluaa sen
+	GLchar const* fragment_shader[] = { fragment_shader_string.c_str() };
+		
 	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glShaderSource(vs, 1, vertex_shader, NULL);
 	glCompileShader(vs);
 	checkCompileErrors(vs, "VERTEX");
 	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glShaderSource(fs, 1, fragment_shader, NULL);
 	glCompileShader(fs);
 	checkCompileErrors(fs, "FRAGMENT");
-
-
 
 	GLuint shader_programme = glCreateProgram();
 	glAttachShader(shader_programme, fs);
