10f8c1b63aa818b9c32b7491a6c22ade66e42390
Alexyyv <thejyva@gmail.com>
2017-12-05 00:00:36 +0200

    Audio added
    ohjeet mainissa audiohommaan, odotellaan tekstien luontia, jotta saa tehtyä sound managerin.

--- MyEngine/main.cpp
+++ MyEngine/main.cpp
@@ -20,9 +20,65 @@
 #include "MikaTestijuttuja.h"
 #include "tilelayer.h"
 #include "group.h"
+#include "ext\gorilla-audio\ga.h"
+#include "ext\gorilla-audio\gau.h"
+#include <stdio.h>
+
+// musat false -> true
+// laita myös alle if 0, tämä korvaa nykyisen mainin (toistaiseksi kunnes fontit on luotu)
+//jos ei käänny, android projun alla on "OpenAL11CoreSDK.exe" setup
+#define Musat 1
+#if Musat 1
+static void setFlagAndDestroyOnFinish(ga_Handle* in_handle, void* in_context)
+{
+	gc_int32* flag = (gc_int32*)(in_context);
+	*flag = 1;
+	ga_handle_destroy(in_handle);
+}
+int main(int argc, char** argv)
+{
+	gau_Manager* mgr;
+	ga_Mixer* mixer;
+	ga_Sound* sound;
+	ga_Handle* handle;
+	gau_SampleSourceLoop* loopSrc = 0;
+	gau_SampleSourceLoop** pLoopSrc = &loopSrc;
+	gc_int32 loop = 0;
+	gc_int32 quit = 0;
+
+	/* Initialize library + manager */
+	gc_initialize(0);
+	mgr = gau_manager_create();
+	mixer = gau_manager_mixer(mgr);
+
+	/* Create and play shared sound */
+	if (!loop)
+		pLoopSrc = 0;
+	sound = gau_load_sound_file("testi.wav", "wav");
+	handle = gau_create_handle_sound(mixer, sound, &setFlagAndDestroyOnFinish, &quit, pLoopSrc);
+	ga_handle_play(handle);
+
+	/* Bounded mix/queue/dispatch loop */
+	while (!quit)
+	{
+		gau_manager_update(mgr);
+		printf("%d / %d\n", ga_handle_tell(handle, GA_TELL_PARAM_CURRENT), ga_handle_tell(handle, GA_TELL_PARAM_TOTAL));
+		gc_thread_sleep(1);
+	}
+
+	/* Clean up sound */
+	ga_sound_release(sound);
+
+	/* Clean up library + manager */
+	gau_manager_destroy(mgr);
+	gc_shutdown();
+
+	return 0;
+}
+#endif
+#if 0
 
 #define TEST_50K_SPRITES 0
-
 
 // Jos haluat printit päälle, tässä 1, jos et valitse 0
 #define DEBUG 1
@@ -33,7 +89,6 @@
 	#define CONSOLE(x)
 	#define CONSOLEND(x)
 #endif
-
 
 int main()
 {
@@ -217,3 +272,4 @@ renderer.end();
 renderer.flush();
 
 */
+#endif
